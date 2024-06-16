23ce9923106717e4ebbe96f4350478ef125f998b
HwangGwiMan <eze886@daum.net>
2024-06-16 18:08:41 +0900

    Spring Config cors csrf

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
+
+
+@Log4j2
 @Controller
+@RequestMapping("/api")
 public class UserController {
-	
-	
+
+	@PostMapping("/join")
+	public ResponseEntity<?> join() {
+		log.info("Test");
+
+		return ResponseEntity.ok().body("JOIN TEST");
+	}
 
 }
